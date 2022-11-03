580ab9d2fbe006e81a813f2b0e9c174c9f923cf4
Bruno Jullien <bruno.jullien@gmail.com>
2022-11-02 19:25:20 -0700

    101. Create Index Page

---
+++ src/main/java/com/bbj/controllers/IndexController.java
@@ -0,0 +1,13 @@
+package com.bbj.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller                                         //make it a Spring Bean
+public class IndexController {
+
+    @RequestMapping({"","/", "/index"})      //when we go to these URLs, getIndexPage() is called
+    public String getIndexPage() {
+        return "index";                             //will try to go against a Thymeleaf template index
+    }
+}
